4c8b133b82707d1156c1244a58f50f0a46e3a48c
Mikhel Lev <levmihel150@gmail.com>
2025-02-28 10:13:56 +0700

    Добавлено сохранение каждого 5 кадра с руками в папку images, убран счетчика кадров чтобы не мешал нейросети находить закономерности

--- handTrackingWithoutBackground.py
+++ handTrackingWithoutBackground.py
@@ -1,6 +1,5 @@
 import cv2
 import mediapipe as mp
-import time
 import keyboard
 
 cap = cv2.VideoCapture(0)
@@ -8,9 +7,7 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
-
-pTime = 0
-cTime = 0
+count = 0
 
 while True:
     bg = cv2.imread('background.jpg')
@@ -18,9 +15,9 @@ while True:
 
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    # print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
+        count += 1
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
                 print(id, lm)
@@ -30,11 +27,9 @@ while True:
                     cv2.circle(bg, (cx, cy), 30, (255, 0, 0), cv2.FILLED)
             mpDraw.draw_landmarks(bg, handLms, mpHands.HAND_CONNECTIONS)
 
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
+        if count % 5 == 0 and count > 0:
+            cv2.imwrite(f'images/{count//5}.jpg', bg)
 
-    cv2.putText(bg, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 3, (255, 0, 255), 3)
 
     cv2.imshow("Image", bg)
     cv2.waitKey(1)
